6823727dc763b6c3d6a4244505bf2acb7d901295
Aleksey Zakharov <aleksey.zakharov@gmail.com>
2018-10-02 15:36:27 -0700

    Initial Commit

---
+++ Stacker/Assets/Scripts/Block.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour {
+
+    public float speed = 5.0f;
+
+    enum dir {Left, Right };
+
+    private bool STOP = false;
+    private dir direction;
+
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //make sure we can move
+        if (!STOP)
+        {
+            //check which direction we are coming from?
+            if (direction == dir.Right)
+            {
+                transform.Translate(new Vector3(0,0,speed*Time.deltaTime));
+            }
+            else
+            {
+                transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+            }
+        }
+	}
+
+    void Direction(bool isRight)
+    {
+        if(isRight)
+            direction = dir.Right;
+        else
+            direction = dir.Left;
+    }
+
+    void Size(int width, int depth)
+    {
+
+    }
+
+    //function that stops this object from transforming
+    void Stop()
+    {
+        STOP = true;
+    }
+
+}
---
+++ Stacker/Assets/Scripts/MainScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainScript : MonoBehaviour {
+
+    public GameObject prefabBlock;
+
+    public GameObject newBlock;
+    public GameObject oldBlock;
+
+    bool isRight;
+
+    Vector3 leftVec = new Vector3(-15, 0, 0);
+    Vector3 rightVec = new Vector3(0, 0, -15);
+
+    // Use this for initialization
+    void Start () {
+        isRight = true;
+        oldBlock.SendMessage("Stop");
+        //spawn intial block
+        spawnBlock();
+        //newBlock.SendMessage("Stop");
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void spawnBlock()
+    {
+        Vector3 blockVec = leftVec;
+        if (isRight)
+            blockVec = rightVec;
+        newBlock = Instantiate(prefabBlock, blockVec, Quaternion.identity);
+        newBlock.SendMessage("Direction", isRight);
+
+        newBlock.transform.parent = GameObject.Find("Stacks").transform;
+
+        if (isRight)
+            isRight = false;
+        else
+            isRight = true;
+    }
+
+    void translateBlocksDown()
+    {
+
+    }
+
+}
